1306f26f30adfa282f2a32b461c71bb8ab67138a
shahab-edraak <shahab@edraaksystems.com>
2023-11-07 15:54:22 +0500

    controllers,middleware and express async handler

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotnet = require("dotenv").config();
 
 const app = express();
+app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
