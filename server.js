1dfcb1f7a958000d971f46fa68738816544f604e
shahab-edraak <shahab@edraaksystems.com>
2023-11-07 16:12:34 +0500

    mongo db connection

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDB = require("./config/dbConnection");
 const dotnet = require("dotenv").config();
-
+connectDB();
 const app = express();
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
